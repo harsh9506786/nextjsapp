5c03d8bd3720e48fc4909619062537a8cc29654c
Harshvardhan Sharma <harsh@28>
2025-09-16 20:46:27 +0530

    testign-autodeployment

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import react from "react"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-amber-600">Home page</h1>
+      <h1 className="text-amber-600 text-4xl border-2 border-amber-600">Home page</h1>
     </div>
   );
 }
